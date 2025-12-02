76e99eeebb592a1a5db6837bc3725111202253d5
Harnimarpreet Singh Bindra <hnps.bindra@gmail.com>
2025-12-02 15:19:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ mongoose
   .catch((err) => console.error("Mongo error:", err));
 
 // --- ROUTES ---
-app.use("/api/auth", authRoutes);
-app.use("/api/rides", rideRoutes);
+app.use("/auth", authRoutes);
+app.use("/rides", rideRoutes);
 
 app.get("/", (req, res) => {
   res.send("CampusCab API is running");
@@ -28,5 +28,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
+
 
 
